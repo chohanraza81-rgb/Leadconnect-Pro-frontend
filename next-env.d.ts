e85b14dd99d9cd90b9157d68199a2e1f33923f2c
chohanraza81-rgb <chohanraza81@gmail.com>
2026-08-05 05:02:39 +0500

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,12 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+declare module 'react-simple-maps' {
+  export const ComposableMap: any;
+  export const Geographies: any;
+  export const Geography: any;
+}
+
+declare module 'react-tooltip' {
+  export const Tooltip: any;
+}
